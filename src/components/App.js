f89762408847156b43ff123ee75a2e70d8c549fc
hwanyb <mnn9502@naver.com>
2022-05-02 22:00:00 +0900

    build

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Home from '../routes/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/:id' element={<Detail />} />
